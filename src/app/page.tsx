ab82c4142c239180bc186e66805e131bfbe9653d
FarhanZizz <iamfarhanaziz@gmail.com>
2026-05-31 03:27:20 +0600

    feat: add Newsletter component for email subscription

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/hero";
+import { Newsletter } from "@/components/newsletter";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Newsletter />
     </div>
   );
 }
