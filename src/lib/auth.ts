596faac458a53105b8d0e5febc4157790c3df947
Faisal Mahmud <foysalnk9095@gmail.com>
2026-05-31 22:00:57 +0600

    feat: configure Google OAuth provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,18 @@ import { prisma } from "@/lib/prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   plugins: [nextCookies()],
 });
