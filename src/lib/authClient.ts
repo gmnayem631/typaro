121ec900b67f36f3efa64c3b87f164f60d8dbd12
Faisal Mahmud <foysalnk9095@gmail.com>
2026-05-31 20:56:55 +0600

    feat: configure Better Auth with credentials provider

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -1,5 +1,10 @@
-// Faisal Bhai replace this with real Better Auth client
+import { createAuthClient } from "better-auth/react";
 
+export const authClient = createAuthClient({
+  //you can pass client configuration here
+});
+
+// TODO: remove mock session api
 export const useSession = () => ({
   data: null as { user: { name: string; image?: string } } | null,
   isPending: false,
