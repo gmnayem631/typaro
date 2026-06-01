d093ec477fdb8de9ad9df1feb355420d350e69da
Faisal Mahmud <foysalnk9095@gmail.com>
2026-06-01 23:42:54 +0600

    feat: add auth check on private routes

--- src/app/blogs/create/page.tsx
+++ src/app/blogs/create/page.tsx
@@ -1,4 +1,11 @@
-export default function CreateBlogPage() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function CreateBlogPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) redirect("/login");
+
   return (
     <div>
       <h1>Add Blog Post</h1>
